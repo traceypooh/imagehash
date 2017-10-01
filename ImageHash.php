27d2d0eaf900f3ebd66973a9948fc12005b7eb66
Tracey Jaquith <tracey@archive.org>
2017-09-30 18:26:12 -0700

    simplifed much further.  now can uncomment a line to output 9x8 jpgs of the imagery used for each hash

--- ImageHash.php
+++ ImageHash.php
@@ -1,87 +1,72 @@
 <?
 
-class ImageHash
-{
-    const SIZE = 8;
-    // this is 'DifferenceHash' from https://github.com/jenssegers/imagehash
-    public static function hasher($resource)
-    {
-        // For this implementation we create a (SIZE+1) x (SIZE) image.
-        $width = static::SIZE + 1;
-        $height = static::SIZE;
+class ImageHash {
+  const SIZE = 8;
 
-        // Resize the image.
-        $resized = imagecreatetruecolor($width, $height);
-        imagecopyresampled($resized, $resource, 0, 0, 0, 0, $width, $height, imagesx($resource), imagesy($resource));
+  /**
+   * Calculate a perceptual hash of an image file.
+   * this is 'DifferenceHash' from https://github.com/jenssegers/imagehash
+   *
+   * @param  filename
+   * @return int 64-bit
+   */
+  public static function hash($filename) {
+    // For this implementation we create a (SIZE+1) x (SIZE) image.
+    $width = static::SIZE + 1;
+    $height = static::SIZE;
 
-        $hash = 0;
-        $one = 1;
-        for ($y = 0; $y < $height; $y++) {
-            // Get the pixel value for the leftmost pixel.
-            $rgb = imagecolorsforindex($resized, imagecolorat($resized, 0, $y));
-            $left = floor(($rgb['red'] + $rgb['green'] + $rgb['blue']) / 3);
+    // Resize the image.
+    $resized = imagecreatetruecolor($width, $height);
+    $resource = imagecreatefromstring(file_get_contents($filename));
+    imagecopyresampled($resized, $resource, 0, 0, 0, 0, $width, $height, imagesx($resource), imagesy($resource));
+    imagedestroy($resource);
 
-            for ($x = 1; $x < $width; $x++) {
-                // Get the pixel value for each pixel starting from position 1.
-                $rgb = imagecolorsforindex($resized, imagecolorat($resized, $x, $y));
-                $right = floor(($rgb['red'] + $rgb['green'] + $rgb['blue']) / 3);
+    // debugging -- uncomment to save resampled 8x9 image
+    // imagejpeg($resized, basename($filename) . '.jpg', 100);
 
-                // Each hash bit is set based on whether the left pixel is brighter than the right pixel.
-                // http://www.hackerfactor.com/blog/index.php?/archives/529-Kind-of-Like-That.html
-                if ($left > $right) {
-                    $hash |= $one;
-                }
+    $hash = 0;
+    $one = 1;
+    for ($y = 0; $y < $height; $y++) {
+      // Get the pixel value for the leftmost pixel.
+      $rgb = imagecolorsforindex($resized, imagecolorat($resized, 0, $y));
+      $left = floor(($rgb['red'] + $rgb['green'] + $rgb['blue']) / 3);
 
-                // Prepare the next loop.
-                $left = $right;
-                $one = $one << 1;
-            }
-        }
+      for ($x = 1; $x < $width; $x++) {
+        // Get the pixel value for each pixel starting from position 1.
+        $rgb = imagecolorsforindex($resized, imagecolorat($resized, $x, $y));
+        $right = floor(($rgb['red'] + $rgb['green'] + $rgb['blue']) / 3);
 
-        // Free up memory.
-        imagedestroy($resized);
+        // Each hash bit is set based on whether the left pixel is brighter than the right pixel.
+        // http://www.hackerfactor.com/blog/index.php?/archives/529-Kind-of-Like-That.html
+        if ($left > $right)
+          $hash |= $one;
 
-        return $hash;
-    }
-
-    /**
-     * Calculate the Hamming Distance.
-     *
-     * @param int $hash1
-     * @param int $hash2
-     * @return int
-     */
-    public static function distance($hash1, $hash2)
-    {
-      $dh = 0;
-      for ($i = 0; $i < (self::SIZE * self::SIZE); $i++) {
-        $k = (1 << $i);
-        if (($hash1 & $k) !== ($hash2 & $k))
-          $dh++;
+        // Prepare the next loop.
+        $left = $right;
+        $one = $one << 1;
       }
-      return $dh;
     }
 
-    /**
-     * Calculate a perceptual hash of an image file.
-     *
-     * @param  mixed $resource GD2 resource or filename
-     * @return int
-     */
-    public static function hash($resource)
-    {
-        $destroy = false;
+    // Free up memory.
+    imagedestroy($resized);
 
-        if (! is_resource($resource)) {
-            $resource = imagecreatefromstring(file_get_contents($resource));
-            $destroy = true;
-        }
+    return $hash;
+  }
 
-        $hash = self::hasher($resource);
-
-        if ($destroy)
-            imagedestroy($resource);
-
-        return $hash;
+  /**
+   * Calculate the Hamming Distance.
+   *
+   * @param int $hash1
+   * @param int $hash2
+   * @return int
+   */
+  public static function distance($hash1, $hash2) {
+    $dh = 0;
+    for ($i = 0; $i < (self::SIZE * self::SIZE); $i++) {
+      $k = (1 << $i);
+      if (($hash1 & $k) !== ($hash2 & $k))
+        $dh++;
     }
+    return $dh;
+  }
 }
