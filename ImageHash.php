294b27733e88d4278519df4b235cea3f627cc291
Tracey Jaquith <tracey@archive.org>
2017-09-30 17:41:21 -0700

    simplified further.  wow, there is not really much there, there...

--- ImageHash.php
+++ ImageHash.php
@@ -1,24 +1,9 @@
 <?
 
-interface Implementation
-{
-    /**
-     * Calculate the hash for the given resource.
-     *
-     * @param  resource $resource
-     * @return int
-     */
-    public function hash($resource);
-}
-
-class DifferenceHash implements Implementation
+class ImageHash
 {
     const SIZE = 8;
-
-    /**
-     * {@inheritDoc}
-     */
-    public function hash($resource)
+    public function hasher($resource)
     {
         // For this implementation we create a 8x9 image.
         $width = static::SIZE + 1;
@@ -57,44 +42,7 @@ class DifferenceHash implements Implementation
 
         return $hash;
     }
-}
 
-
-class ImageHash
-{
-    /**
-     * Return hashes as hexadecimals.
-     */
-    const HEXADECIMAL = 'hex';
-
-    /**
-     * Return hashes as decimals.
-     */
-    const DECIMAL = 'dec';
-
-    /**
-     * The hashing implementation.
-     *
-     * @var Implementation
-     */
-    protected $implementation;
-
-    /**
-     * @var string
-     */
-    protected $mode;
-
-    /**
-     * Constructor.
-     *
-     * @param Implementation $implementation
-     * @param string         $mode
-     */
-    public function __construct(Implementation $implementation = null, $mode = self::HEXADECIMAL)
-    {
-        $this->implementation = $implementation ?: new DifferenceHash;
-        $this->mode = $mode;
-    }
 
     /**
      * Calculate a perceptual hash of an image file.
@@ -107,50 +55,18 @@ class ImageHash
         $destroy = false;
 
         if (! is_resource($resource)) {
-            $resource = $this->loadImageResource($resource);
+            $resource = imagecreatefromstring(file_get_contents($resource));
             $destroy = true;
         }
 
-        $hash = $this->implementation->hash($resource);
+        $hash = $this->hasher($resource);
 
-        if ($destroy) {
-            $this->destroyResource($resource);
-        }
+        if ($destroy)
+            imagedestroy($resource);
 
-        return $this->formatHash($hash);
+        return $hash;
     }
 
-    /**
-     * Calculate a perceptual hash of an image string.
-     *
-     * @param  mixed $data Image data
-     * @return string
-     */
-    public function hashFromString($data)
-    {
-        $resource = $this->createResource($data);
-
-        $hash = $this->implementation->hash($resource);
-
-        $this->destroyResource($resource);
-
-        return $this->formatHash($hash);
-    }
-
-    /**
-     * Compare 2 images and get the hamming distance.
-     *
-     * @param  mixed $resource1
-     * @param  mixed $resource2
-     * @return int
-     */
-    public function compare($resource1, $resource2)
-    {
-        $hash1 = $this->hash($resource1);
-        $hash2 = $this->hash($resource2);
-
-        return $this->distance($hash1, $hash2);
-    }
 
     /**
      * Calculate the Hamming Distance.
@@ -162,17 +78,9 @@ class ImageHash
     public function distance($hash1, $hash2)
     {
         if (extension_loaded('gmp')) {
-            if ($this->mode === self::HEXADECIMAL) {
-                $dh = gmp_hamdist('0x'.$hash1, '0x'.$hash2);
-            } else {
-                $dh = gmp_hamdist($hash1, $hash2);
-            }
-        } else {
-            if ($this->mode === self::HEXADECIMAL) {
-                $hash1 = $this->hexdec($hash1);
-                $hash2 = $this->hexdec($hash2);
-            }
+            $dh = gmp_hamdist($hash1, $hash2);
 
+        } else {
             $dh = 0;
             for ($i = 0; $i < 64; $i++) {
                 $k = (1 << $i);
@@ -183,72 +91,5 @@ class ImageHash
         }
 
         return $dh;
-    }
-
-    /**
-     * Convert hexadecimal to signed decimal.
-     *
-     * @param string $hex
-     * @return int
-     */
-    public function hexdec($hex)
-    {
-        if (strlen($hex) == 16 && hexdec($hex[0]) > 8) {
-            list($higher, $lower) = array_values(unpack('N2', hex2bin($hex)));
-            return $higher << 32 | $lower;
-        }
-
-        return hexdec($hex);
-    }
-
-    /**
-     * Get a GD2 resource from file.
-     *
-     * @param  string $file
-     * @return resource
-     */
-    protected function loadImageResource($file)
-    {
-        try {
-            return $this->createResource(file_get_contents($file));
-        } catch (Exception $e) {
-            throw new InvalidArgumentException("Unable to load file: $file");
-        }
-    }
-
-    /**
-     * Get a GD2 resource from string.
-     *
-     * @param string $data
-     * @return resource
-     */
-    protected function createResource($data)
-    {
-        try {
-            return imagecreatefromstring($data);
-        } catch (Exception $e) {
-            throw new InvalidArgumentException('Unable to create GD2 resource');
-        }
-    }
-
-    /**
-     * Destroy GD2 resource.
-     *
-     * @param resource $resource
-     */
-    protected function destroyResource($resource)
-    {
-        imagedestroy($resource);
-    }
-
-    /**
-     * Format hash in hex.
-     *
-     * @param int $hash
-     * @return string|int
-     */
-    protected function formatHash($hash)
-    {
-        return $this->mode === static::HEXADECIMAL ? dechex($hash) : $hash;
     }
 }
